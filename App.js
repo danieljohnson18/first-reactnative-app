948a16b237d4b195a02e9a41ac88fd7d7078ae84
Daniel Arikawe <arikawedaniel@gmail.com>
2022-02-20 22:44:00 +0100

    looping

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [people, setPeople] = useState([
+    { name: "Daniel", key: 20 },
+    { name: "Johnson", key: 19 },
+    { name: "Arikawe", key: 18 },
+    { name: "Tolu", key: 17 },
+  ]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: "40px",
+    paddingHorizontal: "20px",
   },
 });
